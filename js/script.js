e5dfae38a176c8dcac7a46333fc6ad96d8a5be03
Alexey Tsvirkun <alexey.tsvirkun17@gmail.com>
2022-10-20 23:31:25 +0300

    доделал бургер и плавный скролл

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 const burger = document.querySelector('.menu__icon');
 const menu = document.querySelector('.menu__list');
+const body = document.body;
 
 if(burger && menu) {
 	burger.addEventListener('click', () => {
 		burger.classList.toggle('active')
 		menu.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+
+	menu.querySelectorAll('a').forEach(link => {
+		link.addEventListener('click', () => {
+			burger.classList.toggle('active')
+			menu.classList.toggle('active')
+			body.classList.toggle('lock');
+		})
+	})
+
+	menu.addEventListener('click', e => {
+		if (e.target.classList.contains('menu__list')) {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('lock')
+		}
 	})
 }
+
+// Плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', e => {
+		e.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+})
